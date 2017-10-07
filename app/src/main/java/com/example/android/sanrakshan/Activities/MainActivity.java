83591a2e2027d88518891069a9aeab2cbeed8dd3
sarthakmathur <sarthakmathur97@gmail.com>
2017-10-07 10:52:40 +0530

    initial commit

---
+++ app/src/main/java/com/example/android/sanrakshan/Activities/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.android.sanrakshan.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.example.android.sanrakshan.Fragments.ContactsFragment;
+import com.example.android.sanrakshan.Fragments.PoliceFragment;
+import com.example.android.sanrakshan.Fragments.TipsFragment;
+import com.example.android.sanrakshan.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    FragmentManager fragManager;
+    FragmentTransaction fragTxn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        fragManager = getSupportFragmentManager();
+        final ContactsFragment contactsFragment = new ContactsFragment(this);
+        fragTxn = fragManager.beginTransaction();
+        fragTxn.replace(R.id.fragFrame, contactsFragment);
+        fragTxn.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragTxn.commit();
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.contacts){
+                    fragManager = getSupportFragmentManager();
+                    fragTxn = fragManager.beginTransaction();
+                    fragTxn.replace(R.id.fragFrame, contactsFragment);
+                    fragTxn.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                    fragTxn.commit();
+                }
+
+                if (item.getItemId() == R.id.police){
+                    fragManager = getSupportFragmentManager();
+                    PoliceFragment policeFragment = new PoliceFragment(MainActivity.this);
+                    fragTxn = fragManager.beginTransaction();
+                    fragTxn.replace(R.id.fragFrame, policeFragment);
+                    fragTxn.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                    fragTxn.commit();
+                }
+
+                if (item.getItemId() == R.id.tips){
+                    fragManager = getSupportFragmentManager();
+                    TipsFragment tipsFragment = new TipsFragment(MainActivity.this);
+                    fragTxn = fragManager.beginTransaction();
+                    fragTxn.replace(R.id.fragFrame,tipsFragment );
+                    fragTxn.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                    fragTxn.commit();
+                }
+
+                return true;
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/android/sanrakshan/Fragments/PoliceFragment.java
@@ -0,0 +1,36 @@
+package com.example.android.sanrakshan.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.sanrakshan.R;
+
+/**
+ * Created by HP on 07-Oct-17.
+ */
+
+public class PoliceFragment extends Fragment {
+
+    Context ctx;
+
+    public PoliceFragment(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_police,container,false);
+        return  rootView;
+    }
+}
